7415f0ebd73d5d558ac5883b3ea4a0135a419c26
Djonasus <60808389+Djonasus@users.noreply.github.com>
2024-09-28 16:05:40 +0300

    fps counter

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1727528299|1456493883";
+const CACHE_VERSION = "1727528728|1885117116";
 const CACHE_PREFIX = "Final Signal-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
