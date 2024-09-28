d579b979d36accdf8f027138404591b09505062a
Djonasus <60808389+Djonasus@users.noreply.github.com>
2024-09-28 11:57:28 +0300

    new input

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1727511140|2718269286";
+const CACHE_VERSION = "1727513834|311239352";
 const CACHE_PREFIX = "Final Signal-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
